b4b3aca8f912856c1efe1d877b6b9b9973d95cff
mayamariakjaer <maya_t_kjaer@hotmail.com>
2019-09-18 12:30:05 +0200

    travis file..

---
+++ src/test/java/com/clienthttpTest/www.java
@@ -0,0 +1,12 @@
+package com.clienthttpTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class www {
+    @Test
+    void mathShouldWork(){
+        assertEquals(4, 2+2);
+    }
+}
